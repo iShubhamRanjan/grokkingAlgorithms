1bfed15db59b080f2e438c5d144b1dfe3e8fa470
iShubhamRanjan <34813032+iShubhamRanjan@users.noreply.github.com>
2024-02-09 09:55:00 +0000

    Selection sort + 1 variation

--- sorting.py
+++ sorting.py
@@ -6,5 +6,32 @@ def simple_sort(nums):
   return nums
 
 
+def selection_sort(nums):
+  for i in range(0, len(nums) - 1):
+    smallest = i
+    for j in range(i + 1, len(nums)):
+      if nums[j] < nums[smallest]:
+        smallest = j
+    nums[i], nums[smallest] = nums[smallest], nums[i]
+  return nums
+
+
+def selection_sort_variation(nums):
+  new_nums = []
+  for i in range(0, len(nums)):
+    smallest = find_smallest_helper(nums)
+    new_nums.append(nums.pop(smallest))
+  return new_nums
+
+
+def find_smallest_helper(nums):
+  smallest = nums[0]
+  smallest_index = 0
+  for i in range(len(nums)):
+    if nums[i] < smallest:
+      smallest, smallest_index = nums[i], i
+  return smallest_index
+
+
 nums = [2, 4, 6, 8, 10, 1, 3, 5, 7, 9]
-print(simple_sort(nums))
+print(selection_sort(nums))
