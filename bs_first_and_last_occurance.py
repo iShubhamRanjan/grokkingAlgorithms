9606db64db5b16fdd6ba85123da9084a69474ca8
iShubhamRanjan <34813032+iShubhamRanjan@users.noreply.github.com>
2024-02-12 16:23:38 +0000

    Updated formatting

--- bs_first_and_last_occurance.py
+++ bs_first_and_last_occurance.py
@@ -1,12 +1,13 @@
-arr, x = [8,8,6,6,6,6,6,6,3,2,2,2,0,0,0], 6
+arr, x = [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0], 6
+
 
 def binary_search(lo, hi, condition):
   if lo > hi:
     return -1
-  
+
   mid = (hi + lo) // 2
   result = condition(mid)
-  
+
   if result == 'found':
     return mid
   elif result == 'left':
@@ -14,7 +15,9 @@ def binary_search(lo, hi, condition):
   elif result == 'right':
     return binary_search(mid + 1, hi, condition)
 
+
 def first_occurance():
+
   def condition(mid):
     if arr[mid] == x:
       if (mid - 1) >= 0 and arr[mid - 1] == x:
@@ -22,13 +25,15 @@ def first_occurance():
       else:
         return 'found'
     elif arr[mid] > x:
-      return 'right'  
+      return 'right'
     elif arr[mid] < x:
       return 'left'
-      
+
   return binary_search(0, len(arr) - 1, condition)
 
+
 def last_occurance():
+
   def condition(mid):
     if arr[mid] == x:
       if (mid + 1) <= (len(arr) - 1) and arr[mid + 1] == x:
@@ -36,12 +41,14 @@ def last_occurance():
       else:
         return 'found'
     elif arr[mid] < x:
-      return 'left'  
+      return 'left'
     elif arr[mid] > x:
       return 'right'
-      
+
   return binary_search(0, len(arr) - 1, condition)
 
-def first_and_last_occurance():
-  print(f'first occurance: {first_occurance()}; last occurance: {last_occurance()}')
 
+def first_and_last_occurance():
+  print(
+      f'first occurance: {first_occurance()}; last occurance: {last_occurance()}'
+  )
